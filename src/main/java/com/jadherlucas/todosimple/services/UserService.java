172f7768ff99d9a3d29e7980de04493d55b7b7cf
Jadher <jadherlucas13@gmail.com>
2024-07-09 10:49:31 -0300

    :bug: fix: Correção do UserService

--- src/main/java/com/jadherlucas/todosimple/services/UserService.java
+++ src/main/java/com/jadherlucas/todosimple/services/UserService.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.jadherlucas.todosimple.models.User;
-import com.jadherlucas.todosimple.repositories.TaskRepository;
 import com.jadherlucas.todosimple.repositories.UserRepository;
 
 @Service
@@ -16,37 +15,33 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private TaskRepository taskRepository;
-
-    public User buscarPeloId(Long id) {
+    public User findById(Long id) {
         Optional<User> user = this.userRepository.findById(id);
         return user.orElseThrow(() -> new RuntimeException(
                 "Usuário não encontrado! Id:" + id + ",Tipo: " + User.class.getName()));
     }
 
     @Transactional
-    public User criar(User obj){
+    public User create(User obj){
         obj.setId(null);
         obj = this.userRepository.save(obj);
-        this.taskRepository.saveAll(obj.getTasks());
         return obj;
     }
 
     @Transactional
-    public User editar(User obj){
-        User newObj = buscarPeloId(obj.getId());
+    public User update(User obj){
+        User newObj = findById(obj.getId());
         newObj.setPassword(obj.getPassword());
         return this.userRepository.save(newObj);
     }
 
     @Transactional
-    public void deletar(Long id){
-        buscarPeloId(id);
+    public void delete(Long id){
+        findById(id);
         try {
             this.userRepository.deleteById(id);
         } catch (Exception e) {
-            throw new RuntimeException("Não é possivel excluir pois há entidades relacionadas.");
+            throw new RuntimeException("Não é possível excluir pois há entidades relacionadas!");
         }
     }
 
