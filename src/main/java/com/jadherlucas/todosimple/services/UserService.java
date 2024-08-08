212449b9f12ef8c969ffcaa1cc645d28f271a543
Jadher <jadherlucas13@gmail.com>
2024-08-08 12:18:24 -0300

    :sparkles: feat: Adição da criptografia da senha e criação do ProfileEnum

--- src/main/java/com/jadherlucas/todosimple/services/UserService.java
+++ src/main/java/com/jadherlucas/todosimple/services/UserService.java
@@ -1,17 +1,25 @@
 package com.jadherlucas.todosimple.services;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.jadherlucas.todosimple.models.User;
+import com.jadherlucas.todosimple.models.enums.ProfileEnum;
 import com.jadherlucas.todosimple.repositories.UserRepository;
 import com.jadherlucas.todosimple.services.exceptions.ObjectNotFoundException;
 
+
 @Service
 public class UserService {
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
     private UserRepository userRepository;
@@ -25,6 +33,8 @@ public class UserService {
     @Transactional
     public User create(User obj){
         obj.setId(null);
+        obj.setPassword(this.bCryptPasswordEncoder.encode(obj.getPassword()));
+        obj.setProfiles(Stream.of(ProfileEnum.USER.getCode()).collect(Collectors.toSet()));
         obj = this.userRepository.save(obj);
         return obj;
     }
@@ -33,6 +43,7 @@ public class UserService {
     public User update(User obj){
         User newObj = findById(obj.getId());
         newObj.setPassword(obj.getPassword());
+        obj.setPassword(this.bCryptPasswordEncoder.encode(obj.getPassword()));
         return this.userRepository.save(newObj);
     }
 
